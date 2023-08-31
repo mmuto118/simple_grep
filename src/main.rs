8e5af49d2fc0c8cfc0822cdb35e13e2e034a37f4
mmuto118 <masahiro-muto@intelligent.co.jp>
2023-09-01 08:54:05 +0900

    add run function and introduce Result

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,57 @@
 use std::env;
+use std::process;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args)
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searhing for {}",config.query);
+
+    println!("In file {}", config.filename);
+
+    run(config); 
+
+}
+// myresult を定義
+pub type Myresult = std::result::Result<Config, &'static str>;
+
+
+//config struct
+pub struct Config {
+    query: String,
+    filename: String,
 }
 
+impl Config {
+
+    //new config
+    fn new(args: &[String]) -> Myresult {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config{query, filename})
+    }
+
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)?;
+
+
+    println!("With text:\n{}", contents);
+    Ok(())
+}
